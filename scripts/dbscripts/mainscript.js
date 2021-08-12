fefae1e572db1deca1e0c77e4cfe07f7df84d5db
andre <aburte@senegalsoftware.com>
2021-08-12 15:27:04 -0400

    Add scripts and modify db user forms

---
+++ scripts/dbscripts/mainscript.js
@@ -0,0 +1,14 @@
+// Main scripts here
+
+$('#db_user_form_btn').on('click', function () {
+    if ($('#db_user_form_btn').text('Show Delete DB User Form')) {
+        $('#db_user_form_btn').text('Show Add DB User Form');
+        $('#add_db_user_form').toggle();
+        $('#delete_db_user_form').toggle();
+    } 
+    else if ($('#db_user_form_btn').text('Show Add DB User Form')) {
+        $('#db_user_form_btn').text('Show Delete DB User Form');
+        $('#add_db_user_form').toggle();
+        $('#delete_db_user_form').toggle();
+    }
+});    
